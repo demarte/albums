6dd1633425f1ec6cee8f1fa1c8a1e34625d2becb
Ivan De Martino <ivan_demartino@outlook.com>
2019-06-09 21:42:32 -0300

    criado a classe de list

---
+++ src/components/AlbumList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class AlbumList extends Component {
+
+    componentWillMount() {
+
+    }
+    
+    render() {
+        return (
+            <View>
+                <Text> Albums List </Text>
+            </View>
+        );
+    }
+}
